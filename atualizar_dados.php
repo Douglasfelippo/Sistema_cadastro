8d27ed14e9873a8f831884e8d4d62a50425203af
Douglas <douglas_felippo@hotmail.com>
2024-05-21 00:36:53 -0300

    atualização

--- atualizar_dados.php
+++ atualizar_dados.php
@@ -9,20 +9,20 @@ $bancodedados = "cadastro";
                 die("Conexão falhou: " . $conexao->connect_error);
                     }
 
-
+                $termo_pesquisa ="";
                     if(isset($_POST['pesquisa'])) {
                         $termo_pesquisa = $_POST['pesquisa'];
                     }
 
-// Consulta SQL para buscar os dados no banco de dados
-$sql = "SELECT * FROM falecido WHERE nome_falecido LIKE '%$termo_pesquisa%'";
+                    // Consulta SQL para buscar os dados no banco de dados
+                    $sql = "SELECT * FROM falecido WHERE nome_falecido LIKE '%$termo_pesquisa%'";
 
-$result = $conexao->query($sql);
+                    $result = $conexao->query($sql);
 
 
     if ($result->num_rows > 0) {
-    // Existe pelo menos um registro com o ID especificado
-        $row = $result->fetch_assoc();
+        // Existe pelo menos um registro com o ID especificado
+            $row = $result->fetch_assoc();
     
             // Preencha as variáveis com os dados do banco de dados
             $id = $row["id"];
@@ -32,10 +32,10 @@ $result = $conexao->query($sql);
             $numero = $row["numero"];
             $lote = $row["lote"];
             $junto_com = $row["junto_com"];
-    } 
+            } 
     else {
-        echo "Nenhum registro encontrado com o ID especificado";
-    }
+            echo "Nenhum registro encontrado com o ID especificado";
+         }
 
 $conexao->close();
 
